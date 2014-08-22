eb87ec294f2237412f2a6360f87f732055bf7ebd
Billy Tat <billyt@activestate.com>
2014-08-22 09:45:17 -0700

    Use non-legacy buildpack

--- wsgi.py
+++ wsgi.py
@@ -1,2 +1,7 @@
+import os
 from pyramid.paster import get_app
+from waitress import serve
+
+port = int(os.environ['PORT'])
 application = get_app('development.ini', 'main')
+serve(application, host='0.0.0.0', port=port)
